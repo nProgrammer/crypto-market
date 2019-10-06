eaad47980cb9ac0e418816ffb620ec67bd9b5664
Norbert Wagner <wagner.norbert277@gmail.com>
2019-10-06 15:47:43 +0200

    Init project

---
+++ src/main/kotlin/com/wagner/norbert/cryptomarket/CryptoMarketApplication.kt
@@ -0,0 +1,19 @@
+package com.wagner.norbert.cryptomarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class CryptoMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoMarketApplication>(*args)
+}
+
+@RestController
+class HelloController{
+    @GetMapping
+    fun get() = "Hello world!"
+}
---
+++ src/test/kotlin/com/wagner/norbert/cryptomarket/CryptoMarketApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.wagner.norbert.cryptomarket
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CryptoMarketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
